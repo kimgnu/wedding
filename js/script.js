0ff9cab91a44234df5713bc58f0f9e966bbf823d
kimgnu <kimgnu@yahoo.com>
2020-09-26 23:38:51 +0900

    update photos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-'use strict';
-
-(function () {})();
+function addClickListenerOnImage() {
+  [].forEach.call(document.getElementsByClassName('year-photo'), function (el) {
+    el.addEventListener('click', function (event) {
+      location.href = el.src;
+    });
+  });
+}
 
 function adjustsize() {
   [].forEach.call(
@@ -11,10 +15,56 @@ function adjustsize() {
   );
 }
 
+function changeButtonsVisiblity() {
+  let information = document.getElementById('information');
+  let buttons = document.getElementById('buttons');
+  if (
+    document.body.scrollTop >
+      information.offsetTop + information.scrollHeight ||
+    document.documentElement.scrollTop >
+      information.offsetTop + information.scrollHeight
+  ) {
+    buttons.style.display = 'block';
+  } else {
+    buttons.style.display = 'none';
+  }
+}
+
+function scrollToTop() {
+  let source = document.documentElement.scrollTop;
+  if (source === 0) return;
+  let delta = source / 10;
+  let i = source - delta;
+  let interval = setInterval(function () {
+    if (i < delta) i = 0;
+    window.scrollTo(0, i);
+    i -= delta;
+    if (i < 0) clearInterval(interval);
+  }, 20);
+}
+
+function scrollToBottom() {
+  let footerTop = document.getElementById('footer').offsetTop;
+  let source = document.documentElement.scrollTop;
+  if (source >= footerTop) return;
+  let delta = (footerTop - source) / 10;
+  let i = source + delta;
+  let interval = setInterval(function () {
+    if (i + delta > footerTop) i = footerTop;
+    window.scrollTo(0, i);
+    i += delta;
+    if (i > footerTop) clearInterval(interval);
+  }, 20);
+}
+
 window.onload = function () {
   adjustsize();
 };
 
 window.onresize = function () {
   adjustsize();
+};
+
+window.onscroll = function () {
+  changeButtonsVisiblity();
 };
